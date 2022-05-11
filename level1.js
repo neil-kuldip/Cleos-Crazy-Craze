83a50d38a3a057b467e6fbaed824be6c17227416
neil-kuldip <neilkuldip2425@gmail.com>
2022-05-10 22:21:04 -0400

    style: organization of level1.js

--- level1.js
+++ level1.js
@@ -1,3 +1,6 @@
+// JavaScript file for Level 1
+// Last Modified: 5/10/22
+
 let background = document.getElementsByClassName("background")[0];
 let level = document.getElementById("level");
 let cleo = document.getElementsByClassName("cleo")[0];
@@ -11,6 +14,8 @@ const TOP_MAX = background.getBoundingClientRect().bottom;
 const LEFT_MIN = background.getBoundingClientRect().left;
 const LEFT_MAX = background.getBoundingClientRect().right;
 const MAX_NUMBER_OF_CLICKS = 2;
+
+// Cleo's positioning & rotation
 
 // Taken from GeeksforGeeks.org
 function randomNumber(min, max) { 
@@ -29,11 +34,30 @@ cleo.style.top = `${randomTop}px`;
 cleo.style.left = `${randomLeft}px`;
 cleo.style.transform = `rotate(${randomRotate}deg)`;
 
-//Testing
-console.log(level.clientWidth);
-console.log(level.clientHeight);
-console.log(background.getBoundingClientRect().bottom - background.getBoundingClientRect().top);
-console.log(background.getBoundingClientRect().right - background.getBoundingClientRect().left);
+// Testing
+// console.log(level.clientWidth);
+// console.log(level.clientHeight);
+// console.log(background.getBoundingClientRect().bottom - background.getBoundingClientRect().top);
+// console.log(background.getBoundingClientRect().right - background.getBoundingClientRect().left);
+
+// Countdown Timer
+
+let timer = document.getElementById("timer");
+let levelOneSeconds = 20;
+let levelOneMinutes = 0;
+
+timer.innerHTML = `${String(levelOneMinutes).padStart(2, "0")}:${String(levelOneSeconds).padStart(2, "0")}`;    
+
+const countDown = setInterval(() => {
+    levelOneSeconds--;
+    timer.innerHTML = `${String(levelOneMinutes).padStart(2, "0")}:${String(levelOneSeconds).padStart(2, "0")}`;
+    if (levelOneSeconds <= 0) {
+        timer.innerHTML = "TIME'S UP"
+        failedClicks();
+    }
+}, 1000);
+
+// Game mechanics -- Manipulation of timer and clickCounter variables, user's walkthrough on pass or fail
 
 function failedClicks() {
     if (clickCounter === MAX_NUMBER_OF_CLICKS || levelOneSeconds <= 0) {
@@ -68,17 +92,4 @@ cleo.onclick = () => {
     }
 };
 
-let timer = document.getElementById("timer");
-let levelOneSeconds = 20;
-let levelOneMinutes = 0;
 
-timer.innerHTML = `${String(levelOneMinutes).padStart(2, "0")}:${String(levelOneSeconds).padStart(2, "0")}`;    
-
-const countDown = setInterval(() => {
-    levelOneSeconds--;
-    timer.innerHTML = `${String(levelOneMinutes).padStart(2, "0")}:${String(levelOneSeconds).padStart(2, "0")}`;
-    if (levelOneSeconds <= 0) {
-        timer.innerHTML = "TIME'S UP"
-        failedClicks();
-    }
-}, 1000);
